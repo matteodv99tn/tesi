d64b90154df6a2131d144e9bbbbd74531b24a0f5
Matteo Dalle Vedove <matteodv99tn@gmail.com>
2021-08-08 11:13:02 +0200

    Gnuplotter Configurati i file per creare un file c++ che legge i dati delle simulazioni, dovrà elaborarli per creare dei grafici con gnuplot

---
+++ Gnuplotter/include/draft.h
@@ -0,0 +1,15 @@
+#ifndef _MY_DRAFT_H_
+#define _MY_DRAFT_H_
+
+
+class Draft{
+
+public:
+
+    // Static variables
+    static const char * simulation_path;
+    static const chat * out_path;
+
+};
+
+#endif
